dfdc658a4ed055a2ca761258b8f4ffe8b6c4dd6b
Kevin Bui <khvbui@gmail.com>
2022-06-15 22:44:50 -0700

    Classification Model Selection
    -Tested SVM, Decision Tree,  Naive Bayes, and Logistic Regression
    -Selected SVM
    -Calculated F1 Score
    -Produced Classification report
    -Produced Confusion Matrix

--- main.py
+++ main.py
@@ -4,8 +4,17 @@ import seaborn as sns
 from imblearn.under_sampling import RandomUnderSampler
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import f1_score
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
 
-
+"""
+Preparing the Data
+"""
 df_review = pd.read_csv('IMDB Dataset.csv')
 # print(df_review)
 
@@ -26,7 +35,7 @@ df_review_bal, df_review_bal['sentiment'] = rus.fit_resample(
 # print(df_review_imb.value_counts('sentiment'))
 # print(df_review_bal.value_counts('sentiment'))
 
-# Test 33% of the data
+# Will test using 33% of the data
 train, test = train_test_split(df_review_bal, test_size = 0.33,
                                 random_state=42)
 train_x, train_y = train['review'], train['sentiment']
@@ -34,15 +43,69 @@ test_x, test_y = test['review'], test['sentiment']
 
 # Need to convert movie review text into numerical vectors using 
 # "bag of words" technique
-# Stop word 'english' isn't comprehensive, and may need to be customized
+# Stop word 'english' isn't comprehensive, can improve by tokenization
 tfidf = TfidfVectorizer(stop_words = 'english')
-train_x_vector = tfidf.fit_transform(train_x)
+train_x_vector = tfidf.fit_transform(train_x) # sparse matrix
 # print(train_x_vector)
-print(pd.DataFrame.sparse.from_spmatrix(train_x_vector, index = train_x.index,
-                                    columns = tfidf.get_feature_names_out()))
+# print(pd.DataFrame.sparse.from_spmatrix(train_x_vector, index = train_x.index,
+#                                         columns = tfidf.get_feature_names_out()))
 
+# Don't fit to retain original mean and variance
+test_x_vector = tfidf.transform(test_x)
 
+"""
+Model Selection: Classification Algorithms
+"""
+# Support Vector Machines (SVM)
+# Input = text reviews as numerical vectors
+# Output = sentiment
+svc = SVC(kernel = 'linear')
+svc.fit(train_x_vector, train_y)
+# print(svc.predict(tfidf.transform(['A good movie']))) # positive
+# print(svc.predict(tfidf.transform(['An excellent movie']))) # positive
+# print(svc.predict(tfidf.transform(['I did not like this movie at all']))) # negative
 
+# Decision Tree
+# Input = text reviews as numerical vectors
+# Output = sentiment
+dec_tree = DecisionTreeClassifier()
+dec_tree.fit(train_x_vector, train_y)
 
+# Naive Bayes
+# Input = text reviews as an array of numerical vectors
+# Output = sentiment
+gnb = GaussianNB()
+gnb.fit(train_x_vector.toarray(), train_y)
 
+# Logistic Regression
+# Input = text reviews as numerical vectors
+# Output = sentiment
+log_reg = LogisticRegression()
+log_reg.fit(train_x_vector, train_y)
 
+# Evaluate the Mean accuracy of each model
+# 'Test samples', 'True labels'
+svc.score(test_x_vector, test_y) # 0.84
+dec_tree.score(test_x_vector, test_y) # 0.65
+gnb.score(test_x_vector.toarray(), test_y) # 0.63
+log_reg.score(test_x_vector, test_y) # 0.83
+
+# F1 Score = weighted avg of Precision and Recall 
+# Used when False Negatives and False Positives are important
+# Also helpful with data with imbalanced classes
+f1_score(test_y, svc.predict(test_x_vector),
+            labels = ['positive', 'negative'],
+            average = None) # [0.84671533 0.83464567]
+
+# Classification Report
+classification_report(test_y,
+                        svc.predict(test_x_vector),
+                        labels = ['positive', 'negative'])
+
+#Confusion Matrix
+# [[TP FP] [FN TN]]
+conf_mat = confusion_matrix(test_y,
+                            svc.predict(test_x_vector),
+                            labels = ['positive', 'negative'])
+# [[290  45]
+# [ 60 265]]
